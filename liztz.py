90599e75ec9441d1dee8f6012f60567e92576cf4
Sam <sgriffith@alta3.com>
2020-12-08 17:46:38 +0000

    learning lists

---
+++ liztz.py
@@ -0,0 +1,28 @@
+# Type examples
+#my_cat = "garfield is 2"
+#print(my_cat)
+#print(type(my_cat))
+#print(type(12))
+#print(type(3.14))
+
+"""
+Thinking of a grocery list, it might be:
+
+    - eggs
+    - milk
+    - bread
+    - bacon
+    - cheese
+"""
+
+# my_blank_list = []
+# basic_list = ["item1", "item2"]
+
+groceries = [ "eggs",3.14,"milk","bread","bacon","cheese" ]
+# index        0      1      2         3       4         5  
+
+print(groceries)
+print(type(groceries))
+print(groceries[3])
+print("I need to get " + groceries[0])
+print(str(groceries[1]) + " is something I need to get")
