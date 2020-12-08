8ac86ded71bdd5e4d54bf0988415c0afcd6c8452
Sam <sgriffith@alta3.com>
2020-12-08 16:16:30 +0000

    Day 1 afternoon work

---
+++ inputz.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+print("Do you like cats?")
+print("How about dogs?")
+dog = input("What is your doggy's name?\n")
+print("You said that your dog's name is:", dog)
+print("That's a good name")
