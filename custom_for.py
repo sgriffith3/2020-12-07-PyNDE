8b94c3d4f3a05c7e1d34dc0ef9da17d72b8d4736
Sam Griffith <47530477+sgriffith3@users.noreply.github.com>
2020-12-09 16:25:36 -0500

    Create custom_for.py

---
+++ custom_for.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for x in farms:
+    print("Information is : " + str(x))
+
+
+for num, farm in enumerate(farms):
+    print(f"Farm #{num} is called {farm['name']} and has:")
+    for ag in farm['agriculture']:
+        print(f"    {ag}")
